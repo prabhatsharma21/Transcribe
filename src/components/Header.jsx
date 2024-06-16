70b1a8b3a407e99701b637b372053bc00081c904
prabhatsharma21 <prabhatsharmabest1@gmail.com>
2024-06-17 01:15:13 +0530

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className='flex p-4 items-center justify-between gap-4'>
     <h1 className='font-medium'>Free<span className='text-blue-400 bold'>Scribe</span></h1>
-    <button className='flex items-center gap-2'>
+    <button className=' specialBtn flex items-center gap-2 px-4 px-2 rounded-lg text-blue-400'>
       <p>New</p>
       <i className="fa-solid fa-plus"></i>
     </button>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,11 +5,13 @@ export default function HomePage() {
     <main className='flex-1 p-4 flex flex-col text-center gap-3 sm:gap-4 md:gap-5 justify-center pb-20'>
         <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold font-semibold'>Scribe </span></h1>
         <h3 className='font-medium md:text-lg'>Record <span className='text-blue-400'>&rarr;</span> Transcribe <span className='text-blue-400'>&rarr;</span> Translate </h3>
-        <button>
-            <p>Record</p>
+        <button className='specialBtn flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 px-4 py-2 rounded-xl'>
+            <p className='text-blue-400'>Record</p>
             <i class="fa-solid fa-microphone"></i>
 
         </button>
+        <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>Upload <input className='hidden' type='file' accept='.mp3,.wave' /></label>a mp3 file</p>
+        <p className='italic text-slate-500'>Free now free forever</p>
         </main> 
   )
 }
